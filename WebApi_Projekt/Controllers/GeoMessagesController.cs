01660936f3d1f7d6293319907c73e031c8e0ddec
Calphonar <Richardzejak@hotmail.com>
2021-04-29 14:29:31 +0200

    Scaffolded GeoMessages Into Controller

---
+++ WebApi_Projekt/Controllers/GeoMessagesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi_Projekt.Data;
+using WebApi_Projekt.Models;
+
+namespace WebApi_Projekt.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GeoMessagesController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public GeoMessagesController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/GeoMessages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GeoMessage>>> GetGeoMessages()
+        {
+            return await _context.GeoMessages.ToListAsync();
+        }
+
+        // GET: api/GeoMessages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GeoMessage>> GetGeoMessage(int id)
+        {
+            var geoMessage = await _context.GeoMessages.FindAsync(id);
+
+            if (geoMessage == null)
+            {
+                return NotFound();
+            }
+
+            return geoMessage;
+        }
+
+
+        // POST: api/GeoMessages
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<GeoMessage>> PostGeoMessage(GeoMessage geoMessage)
+        {
+            _context.GeoMessages.Add(geoMessage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGeoMessage", new { id = geoMessage.Id }, geoMessage);
+        }
+    }
+}
