392018bfa3d3247ae6dd6aec3d081f8e7482798e
Calphonar <Richardzejak@hotmail.com>
2021-04-29 13:55:09 +0200

    Created Model and context

---
+++ WebApi_Projekt/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Projekt.Models;
+
+namespace WebApi_Projekt.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<GeoMessage> GeoMessages { get; set; } 
+    }
+}
---
+++ WebApi_Projekt/Models/GeoMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Projekt.Models
+{
+    public class GeoMessage
+    {
+        int Id { get; set; }
+        string Message { get; set; }
+        double Longitude { get; set; }
+        double Latitude { get; set; }
+    }
+}
