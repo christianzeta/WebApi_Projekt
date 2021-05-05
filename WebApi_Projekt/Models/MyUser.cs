87137a845e43ee69631f8957c6cc73b57ed26a45
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-05-05 12:09:38 +0200

    MyUser Table Added Token

--- WebApi_Projekt/Models/MyUser.cs
+++ WebApi_Projekt/Models/MyUser.cs
@@ -10,5 +10,6 @@ namespace WebApi_Projekt.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Token { get; set; }
     }
 }
